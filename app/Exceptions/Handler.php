ff4aec60a055241c2ba04b2b02076100cff54603
Manuel Maisano <manumaisano@gmail.com>
2024-06-17 22:44:04 +0200

    mail

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -26,5 +26,16 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+
     }
+        public function render($request, Throwable $exception)
+        {
+            if ($exception instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException) {
+                return response()->json([
+                    'message' => 'Resource not found'
+                ], 404);
+            }
+    
+            return parent::render($request, $exception);
+        }
 }
